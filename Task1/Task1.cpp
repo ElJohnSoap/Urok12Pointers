66d0b26f47a2612f00786ccc5310524cb258a895
Vadim Artemov <93925370+ElJohnSoap@users.noreply.github.com>
2022-02-18 18:22:26 +0300

    first

---
+++ Task1/Task1.cpp
@@ -0,0 +1,36 @@
+/*. ƒан массив целых чисел. ¬оспользовавшись указател€ми, помен€йте местами элементы массива с четными
+и нечетными индексами (т.е. те элементы массива, которые сто€т на четных местах, помен€йте с элементами,
+которые сто€т на нечетных местах).*/
+#include <iostream>
+using namespace std;
+
+void print(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << *(arr + i) << "\t";
+    cout << endl;
+}
+
+void swapARR(int* pArr, int size)
+{
+    for (int i = 1; i < size; i += 2)
+        swap(pArr[i], pArr[i - 1]);
+}
+
+
+
+int main()
+{
+    srand(time(NULL));
+    const int size = 10;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = rand() % 20;
+    int* pArr = arr;
+    print(pArr, size);
+    
+    swapARR(pArr, size);
+    print(arr, size);
+
+}
+    
