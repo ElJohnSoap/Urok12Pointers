dcdcc487f7f8adc2b2d7bdada96d48e34e621d27
Vadim Artemov <93925370+ElJohnSoap@users.noreply.github.com>
2022-03-03 22:15:30 +0300

    all

---
+++ Task77/Task77.cpp
@@ -0,0 +1,41 @@
+﻿/*Задание 7:
+Написать примитивный калькулятор, пользуясь только указателями.*/
+#include <iostream>
+using namespace std;
+
+void calc(int* ptra, char *sign, int* ptrb)
+{
+	//cout << sign;
+	switch (*sign)
+	{
+	case '*':
+		cout << (*ptra) * (*ptrb);
+		break;
+	case '/':
+		cout << (*ptra) / (*ptrb);
+		break;
+	case '+':
+		cout << (*ptra) + (*ptrb);
+		break;
+	case '-':
+		cout << (*ptra) - (*ptrb);
+		break;
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int a, b;
+	int* pa = &a;
+	int* pb = &b;
+	char sign;
+	char* psign = &sign;
+	cin >> a;
+	cin >> sign;
+	cin >> b;
+	cout << a << " " << sign << " " << b << " = ";
+	calc(pa, psign, pb);
+	return 0;
+}
+
